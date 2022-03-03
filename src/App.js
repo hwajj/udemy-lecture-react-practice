702560bebc396e810c06dc45a4ea9e5840fca811
hwajj <azamwara@gmail.com>
2022-03-03 18:05:40 +0900

    필요없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Expenses from './components/Expenses/Expenses';
-import Hello from './components/Hello';
-
 const App = () => {
   const expenses = [
-    { title: '보험', amount: 120000, date: new Date(2021, 12, 4) },
+    { id: 'e1', title: '보험', amount: 120000, date: new Date(2021, 12, 4) },
 
-    { title: '월세', amount: 200000, date: new Date(2021, 12, 10) },
+    { id: 'e2', title: '월세', amount: 200000, date: new Date(2021, 12, 10) },
 
-    { title: '식비', amount: 300000, date: new Date(2021, 12, 30) },
+    { id: 'e3', title: '식비', amount: 300000, date: new Date(2021, 12, 30) },
 
-    { title: '헬스장', amount: 50000, date: new Date(2022, 1, 4) },
+    { id: 'e4', title: '헬스장', amount: 50000, date: new Date(2022, 1, 4) },
   ];
 
   return (
