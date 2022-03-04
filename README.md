91022a0b4845d5906074ddf3b05bda126abc0290
hwajj <azamwara@gmail.com>
2022-03-04 17:53:54 +0900

    다수의 state에 접근하는 방법

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # udemy-lecture-react-practice
+
+## UseState
+
+### 다수의 state에 접근하는 방법
+
+1. 3개의 state
+
+```
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const titleChangeHandler = (event) => {
+      setEnteredTitle(event.target.value);
+    }
+```
+
+2. 1개의 state
+
+- 1개의 state가지도록 useState에 객체 넣는 방법
+- 하나의 state에 접근하면서 다른 데이터 사라지지않도록
+  기존 data 복사한다. 스프레드 연산자 사용 하여 ...userInput
+- 이전 snapshot에 의존, 기존 값 복사한다음에 덮어쓰기
+- state업데이트할때마다 이전 state에 의존해야함
+
+  ```
+   const [userInput, setUserInput] = useState({
+      enteredTitle: '',
+      enteredAmount: '',
+      enteredDate: '',
+  });
+  setUserInput({
+    ...userInput,
+    enteredTitle: event.target.value,
+  });
+  ```
+
+### 방법 3
+
+- state 업데이트 폼의 대안
+
+```
+  const titleChangeHandler = (event) => {
+
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+
+```
+
+- 동시에 많은 업데이트 계획하는 경우 잘못된 snapshow에 의존하게 될 수 있다
+- setUserInput안에 함수넣는 경우는 userState 함수가 제공하는 상태 snapshot이 가장 최신상태 snapshot`임을 부장
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,73 @@
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  //   const [enteredTitle, setEnteredTitle] = useState('');
+  //   const [enteredAmount, setEnteredAmount] = useState('');
+  //   const [enteredDate, setEnteredDate] = useState('');
+
+  //useState에 객체를 넘겨준다
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: '',
+  });
+  const titleChangeHandler = (event) => {
+    //  setEnteredTitle(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((prevState, fx) => {
+      console.log(prevState);
+      console.log(fx);
+
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    //  setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    //  setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="100"
+            step="100"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-03-04" max="2024-12-31" />
+          <input
+            type="date"
+            min="2022-03-04"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
