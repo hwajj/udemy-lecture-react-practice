645cf75a907acd5c6147306811791bf4c32cc94f
hwajj <azamwara@gmail.com>
2022-03-04 18:15:49 +0900

    양방향 바인딩

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,11 +7,11 @@ const ExpenseForm = () => {
   const [enteredDate, setEnteredDate] = useState('');
 
   //useState에 객체를 넘겨준다
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: '',
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
 
@@ -35,18 +35,31 @@ const ExpenseForm = () => {
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+    console.log(expenseData);
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -54,6 +67,7 @@ const ExpenseForm = () => {
             type="number"
             min="100"
             step="100"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -61,6 +75,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2022-03-04"
             max="2024-12-31"
             onChange={dateChangeHandler}
@@ -68,7 +83,9 @@ const ExpenseForm = () => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit"> Add Expense</button>
+        <button type="submit" onClick="">
+          Add Expense
+        </button>
       </div>
     </form>
   );
