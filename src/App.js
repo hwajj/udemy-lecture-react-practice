00f19d6ee94d938094923b0c44c0cbfa0eb251ba
Jeong hwajeong <azamwara@gmail.com>
2022-07-14 15:32:09 +0900

    Refactor code
    Splitting components into multiple components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
-    { id: '1', titile: '휴지', amount: 20000, date: new Date(2022, 7, 12) },
-    { id: '2', titile: '계란', amount: 9000, date: new Date(2022, 7, 12) },
-    { id: '3', titile: '우유', amount: 6000, date: new Date(2022, 7, 12) },
+    { id: '1', title: '휴지', amount: 20000, date: new Date(2022, 7, 12) },
+    { id: '2', title: '계란', amount: 9000, date: new Date(2022, 7, 13) },
+    { id: '3', title: '우유', amount: 6000, date: new Date(2022, 7, 14) },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
