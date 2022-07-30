f51ef773250b1e0cbd518e2b08b16a495d430afd
Jeong hwajeong <azamwara@gmail.com>
2022-07-30 22:53:45 +0900

    Add form to Food order app

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='음식이 차려진 뷔페 테이블' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
