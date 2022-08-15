77d2a7eedf9e4bb80da57dd3d7dac15f56408934
Jeong hwajeong <azamwara@gmail.com>
2022-08-15 17:42:07 +0900

    Add fetched data from firebase

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://react-http-9393c-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
+      );
+      const responseData = await response.json();
+      console.log(response);
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals();
+  }, []); //의존성 없음 , 컴포넌트 최초 실행시에만 실행
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p> Loading...</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
